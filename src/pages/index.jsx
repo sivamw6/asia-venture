40cb338c901a49827c9d6bcb3acf26e47df32a06
sivamw6 <105441569+sivamw6@users.noreply.github.com>
2023-06-05 17:09:15 +1000

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -34,8 +34,10 @@ function HomePage ({images, imageLoaderImage}) {
             <Col className='d-flex justify-content-center'>
               <Row>
                 <Col>
+                   <Container>
                   <p className='p-4'>Welcome to Asia Venture, your ultimate guide to adventuring through the captivating and diverse landscapes of Asia. Our website is designed to provide you with all the inspiration and information you need to embark on an unforgettable journey across this enchanting continent. 
                   Asia is a land of incredible diversity, offering a rich tapestry of cultures, breathtaking natural wonders, and ancient traditions waiting to be explored. From the bustling streets of Tokyo to the serene temples of Kyoto in Japan, the vibrant markets of Bangkok in Thailand, and the majestic beauty of Taiwan, Asia is a treasure trove of extraordinary experiences.</p>
+                  </Container>
                 </Col>
                 <Col className='d-flex justify-content-center align-items-center'>
                   <div style={{ position:'relative', width: '600px', height: '300px'}}><ImageLoader image={imageLoaderImage}/></div>
